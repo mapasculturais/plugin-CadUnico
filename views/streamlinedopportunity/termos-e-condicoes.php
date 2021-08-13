6662467755d09e58d12104391c2e3c7aaaa40a30
erleibiazzio <erleibiazzio@gmail.com>
2021-08-13 14:23:22 -0300

    Adiciona arquivo de termos e condições

---
+++ views/streamlinedopportunity/termos-e-condicoes.php
@@ -0,0 +1,175 @@
+<?php
+    use MapasCulturais\App;
+    $app = App::i();
+    $slug = $this->controller->plugin->slug;
+    $this->jsObject['registrationId'] = $registration_id;
+
+?>
+<section class="termos" >
+    <p class="termos--summay">De acordo com os critérios da LEI Nº 14.017 com sanção presidencial em 29 DE JUNHO DE 2020. Para ter acesso ao Auxílio Emergencial Cultural, você deve cumprir os seguintes requisitos: </p>
+
+    <h2>
+        Termos e Condições: Inciso I<br/>
+        Trabalhadoras e trabalhadores<br/>
+        
+    </h2>
+
+    <div class="termos--list">
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO ATUAÇÃO NO SETOR CULTURAL E FONTE DE RENDA, CONFORME LEI FEDERAL Nº 14.017, DE 29 DE JUNHO DE 2020, QUE DISPÕE SOBRE AÇÕES EMERGENCIAIS DESTINADAS AO SETOR CULTURAL A SEREM ADOTADAS DURANTE O ESTADO DE CALAMIDADE PÚBLICA. 
+                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE ATUEI SOCIAL OU PROFISSIONALMENTE NAS ÁREAS ARTÍSTICA E CULTURAL NOS 24 (VINTE E QUATRO) MESES IMEDIATAMENTE ANTERIORES À 29 DE JUNHO DE 2020, CONFORME INCISO I DO ART. 6º DA LEI FEDERAL Nº 14.017/2020, E ARTIGO 4º, INCISO I DO DECRETO FEDERAL Nº 10.464/2020.
+                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE NÃO POSSUO EMPREGO FORMAL ATIVO COM CONTRATO DE TRABALHO FORMALIZADO NOS TERMOS DA CONSOLIDAÇÃO DAS LEIS DO TRABALHO, CONFORME O INCISO II DO ART. 6º DA LEI FEDERAL Nº 14.017/2020, E O ART. 4º, INCISO II DO DECRETO FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE NÃO SOU TITULAR DE BENEFÍCIO PREVIDENCIÁRIO OU ASSISTENCIAL DO GOVERNO FEDERAL, EXCETO DO PROGRAMA BOLSA FAMÍLIA, CONFORME INCISO III DO ART. 6º DA LEI FEDERAL Nº 14.017/2020, E ART. 4º, INCISO III DO DECRETO FEDERAL Nº 10.464/2020.
+                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE NÃO ESTOU RECEBENDO BENEFÍCIO DO SEGURO DESEMPREGO OU PROGRAMA DE TRANSFERÊNCIA DE RENDA FEDERAL, EXCETO DO PROGRAMA BOLSA FAMÍLIA, CONFORME INCISO III DO ART. 6º DA LEI Nº 14.017/2020, E ART. 4º, INCISO III DO DECRETO FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO RENDA FAMILIAR PER CAPITA DE ATÉ MEIO SALÁRIO MÍNIMO OU RENDA FAMILIAR TOTAL DE ATÉ TRÊS SALÁRIOS MÍNIMOS, CONFORME INCISO IV DO ART. 6º DA LEI FEDERAL Nº 14.017/2020, E ART. 4º, INCISO IV DO DECRETO FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE NÃO RECEBI, NO ANO DE 2018, RENDIMENTOS TRIBUTÁVEIS ACIMA DE R$ 28.559,70 (VINTE E OITO MIL, QUINHENTOS E CINQUENTA E NOVE REAIS E SETENTA CENTAVOS), CONFORME INCISO V DO ART. 6º DA LEI FEDERAL Nº 14.017/2020, E ART. 4º, INCISO V DO DECRETO FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO NÃO SER BENEFICIÁRIO (A) DO AUXÍLIO EMERGENCIAL PREVISTO NA LEI Nº 13.982, DE 2 DE ABRIL DE 2020, EM CONFORMIDADE COM O INCISO VII DO ART. 6º DA LEI Nº 14.017/2020, E ARTIGO 4º, INCISO VII DO DECRETO FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE NÃO SOU AGENTE PÚBLICO, INDEPENDENTEMENTE DA RELAÇÃO JURÍDICA, INCLUSIVE OCUPANTE DE CARGO OU FUNÇÃO TEMPORÁRIO OU DE CARGO EM COMISSÃO DE LIVRE NOMEAÇÃO E EXONERAÇÃO OU, AINDA, TITULAR DE MANDATO ELETIVO, CONFORME ART. 4º, §2º DO DECRETO Nº FEDERAL Nº 10.464/2020.                </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE ESTOU CIENTE DE QUE, EM CASO DE UTILIZAÇÃO DE QUALQUER MEIO ILÍCITO, IMORAL OU DECLARAÇÃO FALSA PARA A PARTICIPAÇÃO DESTE CREDENCIAMENTO, INCORRO NA PENALIDADE PREVISTA NO ARTIGO 299 DO DECRETO LEI Nº 2.848, DE 07 DE DEZEMBRO DE 1940 (CÓDIGO PENAL).                 </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                DECLARO QUE ESTOU CIENTE DA CONCESSÃO DAS INFORMAÇÕES POR MIM DECLARADAS NESTE FORMULÁRIO PARA VALIDAÇÃO EM OUTRAS BASES DE DADOS OFICIAIS.                 </span>
+            </label>
+        </div>
+        <div class="term">
+            <span class="term--box"></span>
+            <label class="term--label">
+                <input type="checkbox" class="term--input" />
+                <span class="termos--text">
+                CONCORDO COM A INCLUSÃO DAS INFORMAÇÕES POR MIM DECLARADAS NESTE FORMULÁRIO NA BASE DE DADOS DA PLATAFORMA MAPA CULTURAL DE MATO GROSSO DO SUL.                </span>
+            </label>
+        </div>
+    </div>
+
+    <nav class="termos--nav-terms">
+        <button class="btn btn-large btn-lab js-btn">Continuar</button>
+    </nav>
+
+    <div id="modalAlert" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2 class="modal-content--title title-modal">Atenção!</h2>
+            <p>
+                Você precisa aceitar todos os termos para continuar com a inscrição no auxílio emergencial da cultura.
+            </p>
+            <button id="btn-close" class="btn">OK</button>
+        </div>
+    </div>
+
+</section>
+
+<script>
+    var span          = document.getElementsByClassName("close")[0];
+    var modal         = document.getElementById("modalAlert");
+    var btnClose      = document.getElementById("btn-close");
+    var btnProsseguir = document.querySelector(".js-btn");
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if(event.target == btnProsseguir){
+            goToNextPage();
+        }else{
+            if(modal.style.display == 'flex'){
+                modal.style.display = "none";
+            }
+        }
+
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    btnClose.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    function goToNextPage() {
+        var checkboxes        = document.querySelectorAll('input[type="checkbox"]');
+        var checkboxesChecked = document.querySelectorAll('input[type="checkbox"]:checked');
+
+        if(checkboxes.length === checkboxesChecked.length){
+            //redirect to next page
+            document.location = MapasCulturais.createUrl($slug, 'aceitar_termos', [MapasCulturais.registrationId])
+        }else{
+            modal.style.display = "flex";
+        }
+    }
+
+</script>

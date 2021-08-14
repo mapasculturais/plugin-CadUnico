0dd62f58fb852de3b51a0d66b3f2b1d568c4e68d
erleibiazzio <erleibiazzio@gmail.com>
2021-08-13 21:21:52 -0300

    Insere internacionalização e corrige identações

--- layouts/parts/streamlinedopportunity/footer.php
+++ layouts/parts/streamlinedopportunity/footer.php
@@ -1,50 +1,53 @@
 <?php
+
+use MapasCulturais\i;
+
 $app = \MapasCulturais\App::i();
 $config = $app->plugins['StreamlinedOpportunity']->config;
 $controller = $app->controller($config['slug']);
 $linkSuporte      = isset($config['link_suporte']) ? $config['link_suporte'] : '';
 $termosECondicoes = isset($config['privacidade_termos_condicoes']) ? $config['privacidade_termos_condicoes'] : $app->createUrl('auth', '', array('termos-e-condicoes'));
-$logotipo = isset($config['logotipo_instituicao']) ? $config['logotipo_instituicao'] : '';?>
+$logotipo = isset($config['logotipo_instituicao']) ? $config['logotipo_instituicao'] : ''; ?>
 
 </section>
 
-<?php if ($linkSuporte){
-    ?>
+<?php if ($linkSuporte) {
+?>
     <div class="support">
-        Precisa de ajuda? <a target="_blank" class="link" href="<?= $linkSuporte; ?> ">Clique aqui</a>
+        <?= i::__(' Precisa de ajuda?', 'streamlined-opportunity') ?> <a target="_blank" class="link" href="<?= $linkSuporte; ?> "> <?= i::__('Clique aqui', 'streamlined-opportunity') ?></a>
     </div>
-    <?php
-}?>
+<?php
+} ?>
 
 <footer id="main-footer">
 
-    <?php if ($logotipo){
-        ?>
-       <div class="logo-state">
+    <?php if ($logotipo) {
+    ?>
+        <div class="logo-state">
             <img src="<?= $logotipo ?>">
         </div>
-        <?php
-    }?>
+    <?php
+    } ?>
 
-    <?php if ($termosECondicoes){
-        ?>
+    <?php if ($termosECondicoes) {
+    ?>
         <a target="_blank" class="terms-conditions" href="<?= $termosECondicoes; ?> ">
-            Politica de Privacidade e termos de condições de uso
+            <?= i::__('Politica de Privacidade e termos de condições de uso', 'streamlined-opportunity') ?>
         </a>
-        <?php
-    }?>
+    <?php
+    } ?>
 
     <div class="credits">
         <a href="https://github.com/mapasculturais/mapasculturais" target="_blank">
-        Software livre Mapas Culturais
-        </a> 
-        <span> por </span> 
+            <?= i::__('Software livre Mapas Culturais', 'streamlined-opportunity') ?>
+        </a>
+        <span> por </span>
 
         <a href="https://hacklab.com.br/" class="hacklab" target="_blank" style="white-space: nowrap;">
-            hacklab <span>/</span>
+            <?= i::__('hacklab', 'streamlined-opportunity') ?> <span>/</span>
         </a>
 
-        <span> e comunidade </span>
+        <span> <?= i::__('e comunidade', 'streamlined-opportunity') ?> </span>
     </div>
 </footer>
 

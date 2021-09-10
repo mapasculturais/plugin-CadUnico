a94bd39f2245e99a2cbe075f98d842a8b169e769
erleibiazzio <erleibiazzio@gmail.com>
2021-09-10 14:23:37 -0300

    Ajusta layout de exibição das declarações iniciais na ficha de inscrição

--- layouts/parts/streamlinedopportunity/initial-statements.php
+++ layouts/parts/streamlinedopportunity/initial-statements.php
@@ -3,17 +3,19 @@
 use MapasCulturais\i;
 ?>
 
-<article>
-    <h4><?=i::__('Declarações iníciais', 'streamlined-opportunity')?></h4>
-    <ul class="initial-statements">
-        <?php foreach($terms['items'] as $value){?>
-            <?php $result = explode("</strong>", $value);?>
-            <li>
-                <small>
-                    <i class="fas fa-check"></i>                    
-                    <?=strip_tags($result[0])?>
-                </small>
-            </li>
-        <?php } ?>
-    </ul>
-</article>
+<div class="registration-fieldset">
+    <h4><?=i::__('Declarações iniciais aceitas pelo proponente', 'streamlined-opportunity')?></h4>
+    <div class="registration-list">
+        <ul class="initial-statements">
+            <?php foreach($terms['items'] as $value){?>
+                <?php $result = explode("</strong>", $value);?>
+                <li>
+                    <small>
+                        <i class="fas fa-check"></i>                    
+                        <?=strip_tags($value)?>
+                    </small>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+</div>

dd990840adea5692ffa15ce486a1c8526e11ff14
erleibiazzio <erleibiazzio@gmail.com>
2022-12-05 10:38:04 -0300

    Define o botão diretamente no template Ref.: #1

--- layouts/parts/cadunico/button-registration.php
+++ layouts/parts/cadunico/button-registration.php
@@ -13,16 +13,17 @@ $url = $this->controller->createUrl('novaInscricao', ['agent' => $app->user->pro
 ?>
 
 <button onclick="location.href='<?= $url ?>'" clickable id="option3" class="informative-box lab-option">
-<div class="informative-box--icon">
-    <i class="fas fa-user"></i>
-</div>
+    <div class="informative-box--icon">
+        <i class="fas fa-user"></i>
+    </div>
 
-<div class="informative-box--title">
-    <h2><?= $plugin->text('dashboard.button') ?: $opportunity->name ?></h2>
-    <i class="fas fa-minus"></i>
-</div>
+    <div class="informative-box--title">
+        <h2><?= $plugin->text('dashboard.button') ?: $opportunity->name ?></h2>
+        <i class="fas fa-minus"></i>
+    </div>
 
-<div class="informative-box--content active" data-content="">
-    <span class="more"> <?= $plugin->text('dashboard.moreInformation'); ?> </span>
+    <div class="informative-box--content active" data-content="">
+        <span class="more"> <?= $plugin->text('dashboard.moreInformation'); ?> </span>
 
-</div>
+    </div>
+</button>
--- layouts/parts/cadunico/govbr-sing-in.php
+++ layouts/parts/cadunico/govbr-sing-in.php
@@ -7,17 +7,19 @@
  * @var CadUnico\Plugin $plugin 
  */
 
-
 $url = $app->createUrl("autenticacao", "govbr");
 ?>
-<div class="informative-box--icon">
-    <i class="fas fa-user"></i>
-</div>
 
-<div class="informative-box--title">
-    <img src="<?=$this->asset("img/sing-in-govbr.png", false)?>" style="width: 25%">
-</div>
+<button onclick="location.href='<?= $url ?>'" clickable id="option3" class="informative-box lab-option">
+    <div class="informative-box--icon">
+        <i class="fas fa-user"></i>
+    </div>
 
-<div class="informative-box--content active" data-content="">
-    <span class="more"> <?= $plugin->text('dashboard.moreInformation'); ?> </span>
-</div>
+    <div class="informative-box--title">
+        <img src="<?=$this->asset("img/sing-in-govbr.png", false)?>" style="width: 25%">
+    </div>
+
+    <div class="informative-box--content active" data-content="">
+        <span class="more"> <?= $plugin->text('dashboard.moreInformation'); ?> </span>
+    </div>
+</button>
